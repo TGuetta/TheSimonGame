74425b6339d1d46dfd90aba8f3894ee1dd72e318
Tomer Gueta <tgtelhai@gmail.com>
2024-12-04 17:25:02 +0200

    answer of step 4

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
-//*********  Step 2 - Create A New Pattern: *****************
+//Step 2 - Create A New Pattern:
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+
+var userClickedPattern = [];
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
 
@@ -9,7 +12,7 @@ function nextSequence() {
 
   gamePattern.push(randomChosenColour);
 
-  //******** Step 3 - Show the Sequence to the User with Animations and Sounds: ********
+  //Step 3 - Show the Sequence to the User with Animations and Sounds:
 
   $("#" + randomChosenColour)
     .fadeIn(100)
@@ -18,4 +21,18 @@ function nextSequence() {
 
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
+
+  $("button").on("click", function () {
+    var userChosenColour = $(this).text();
+  });
 }
+
+//Step 4 - Check Which Button is Pressed:
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+
+  //console.log(userClickedPattern);
+});
